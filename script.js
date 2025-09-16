c854116c1d34c5d5145063a07944779b5c72c92b
Julien Baouz <baouzjulien@gmail.com>
2025-09-16 21:49:50 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+  // Quand on clique sur un lien .card-links a
+  document.querySelectorAll(".card-links a").forEach(link => {
+    link.addEventListener("click", function(e) {
+      // empêcher le saut de page par défaut
+      e.preventDefault();
+      
+      // identifier la section cible
+      const target = document.querySelector(this.getAttribute("href"));
+      
+      // masquer toutes les sections
+      document.querySelectorAll("section").forEach(sec => sec.style.display = "none");
+      
+      // afficher la section cible
+      target.style.display = "block";
+      
+      // la masquer après 3 secondes
+      setTimeout(() => {
+        target.style.display = "none";
+      }, 2000);
+    });
+  });
